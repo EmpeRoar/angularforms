48111737515afb071404eaa4cd8d897985630ae6
Juliusb <Juliusb@ZYLUN-PC-0308>
2017-03-14 23:17:01 +0800

    initial

---
+++ angularforms/app/EmployeeForm/efService.js
@@ -0,0 +1,15 @@
+﻿angularFormsApp.factory('efService',
+    function () {
+        return {
+            employee: {
+                fullName: "Julius Bacosa",
+                notes: "This is a note",
+                department: "Administration",
+                perkCar: true,
+                perkStock: false,
+                perkSixWeeks: true,
+                payrollType: "none"
+            }
+        }
+
+    });
